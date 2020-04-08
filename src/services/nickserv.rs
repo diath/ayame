2af13ed8f2a093156241f01f54554de3289f5dfa
diath <contact@diath.net>
2020-04-08 14:17:48 +0200

    Improve NickServ service (#84)

--- src/services/nickserv.rs
+++ src/services/nickserv.rs
@@ -12,6 +12,12 @@ pub struct NickServ {
 }
 
 impl NickServ {
+    pub fn new() -> NickServ {
+        NickServ {
+            nicks: Mutex::new(HashMap::new()),
+        }
+    }
+
     async fn reply(&self, client: &Client, message: &str) {
         let nick = client.nick.lock().await;
         client
@@ -35,17 +41,28 @@ impl Service for NickServ {
                     if self.nicks.lock().await.contains_key(params[1]) {
                         self.reply(client, "Nick already taken").await;
                     } else {
-                        self.nicks
-                            .lock()
-                            .await
-                            .insert(params[1].to_string(), params[2].to_string());
-                        self.reply(client, "Nick successfully registered").await;
+                        let nick = client.nick.lock().await.to_string();
+                        if nick == params[1] {
+                            self.nicks
+                                .lock()
+                                .await
+                                .insert(params[1].to_string(), params[2].to_string());
+                            self.reply(client, "Nick successfully registered").await;
+                        } else {
+                            self.reply(client, "You can only register your current nick")
+                                .await;
+                        }
                     }
                 }
             }
             "identify" => {
-                if let Some(password) = self.nicks.lock().await.get(params[1]) {
+                if params.len() < 3 {
+                    self.reply(client, "Not enough params").await;
+                } else if *client.identified.lock().await {
+                    self.reply(client, "You are already identified").await;
+                } else if let Some(password) = self.nicks.lock().await.get(params[1]) {
                     if password == params[2] {
+                        (*client.identified.lock().await) = true;
                         self.reply(client, "You are now identified for this nick")
                             .await;
                     } else {
@@ -55,8 +72,50 @@ impl Service for NickServ {
                     self.reply(client, "Nick not registered").await;
                 }
             }
+            "logout" => {
+                let identified = *client.identified.lock().await;
+                if identified {
+                    (*client.identified.lock().await) = false;
+                    self.reply(client, "You are no longer identified").await;
+                } else {
+                    self.reply(client, "You are not identified").await;
+                }
+            }
+            "drop" => {
+                if params.len() < 3 {
+                    self.reply(client, "Not enough params").await;
+                } else if *client.identified.lock().await {
+                    self.reply(client, "You must logout before dropping a nick")
+                        .await;
+                } else {
+                    let mut password = None;
+                    if let Some(_password) = self.nicks.lock().await.get(params[1]) {
+                        password = Some(_password.clone());
+                    }
+
+                    if let Some(password) = password {
+                        if password == params[2] {
+                            self.nicks.lock().await.remove(params[1]);
+                            self.reply(client, "The nick registration has been released")
+                                .await;
+                        } else {
+                            self.reply(client, "Wrong password").await;
+                        }
+                    } else {
+                        self.reply(client, "Nick not registered").await;
+                    }
+                }
+            }
+            "help" => {
+                self.reply(client, "NickServ commands:").await;
+                self.reply(client, "REGISTER <nick> <password>").await;
+                self.reply(client, "IDENTIFY <nick> <password>").await;
+                self.reply(client, "LOGOUT").await;
+                self.reply(client, "DROP <nick> <password>").await;
+                self.reply(client, "HELP").await;
+            }
             _ => {
-                self.reply(client, "Unknown command").await;
+                self.reply(client, "Unknown command, try HELP").await;
             }
         }
     }
